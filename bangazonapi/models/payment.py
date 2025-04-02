cd8f731ad57cee3eb1338e7edd047af24d8fc1d5
Brian Henry <brianhenrytech@gmail.com>
2025-04-02 13:34:06 -0500

    Bugfix: Incorrect result for minimum products sold

--- bangazonapi/models/payment.py
+++ bangazonapi/models/payment.py
@@ -3,10 +3,13 @@ from .customer import Customer
 from safedelete.models import SafeDeleteModel
 from safedelete.models import SOFT_DELETE
 
+
 class Payment(SafeDeleteModel):
 
     _safedelete_policy = SOFT_DELETE
-    merchant_name = models.CharField(max_length=25,)
+    merchant_name = models.CharField(
+        max_length=25,
+    )
     account_number = models.CharField(max_length=25)
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="payment_types")
 
