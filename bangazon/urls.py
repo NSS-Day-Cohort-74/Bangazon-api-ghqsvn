21e09db7977cde75a438c61283d218fd2e4f5c68
Brian Henry <brianhenrytech@gmail.com>
2025-04-04 10:28:07 -0500

    Fixes order filtering

--- bangazon/urls.py
+++ bangazon/urls.py
@@ -15,7 +15,6 @@ router.register(r"customers", Customers, "customer")
 router.register(r"users", Users, "user")
 router.register(r"orders", Orders, "order")
 router.register(r"cart", Cart, "cart")
-router.register(r"stores", Stores, "store")
 
 router.register(r"payment-types", Payments, "payment")
 router.register(r"profile", Profile, "profile")
--- bangazonapi/models/store.py
+++
@@ -1,9 +0,0 @@
-
-from django.db import models
-from .customer import Customer
-
-
-class Store(models.Model):
-    customer = models.ForeignKey(Customer, related_name='store', on_delete=models.CASCADE,)
-    created_on = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=255)
