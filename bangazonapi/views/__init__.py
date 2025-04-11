2b96a5fa84ca002bbe30db1e1af17af86987e9a4
Brian Henry <brianhenrytech@gmail.com>
2025-04-11 09:34:16 -0500

    Fixed images for reports by actually Serializing the products like i should have

--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -2,7 +2,7 @@ from .register import register_user
 from .register import login_user
 from .paymenttype import Payments, PaymentSerializer
 from .order import Orders
-from .product import Products
+from .product import Products, ProductSerializer
 from .cart import Cart
 from .profile import Profile
 from .productcategory import ProductCategories
--- bangazonapi/views/report.py
+++ bangazonapi/views/report.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from bangazonapi.models.product import Product
+from bangazonapi.views import ProductSerializer
 from django.shortcuts import render
 
 from django.urls import reverse
@@ -9,17 +10,19 @@ from django.urls import reverse
 def report(request):
     if request.path == "/reports/expensiveproducts":
         products = Product.objects.filter(price__gte=1000)
+        serialized_products = ProductSerializer(products, many=True)
         context = {
             "title": "20 most Expensive products",
             "heading": "20 most Expensive products",
-            "pricey_products": products,
+            "pricey_products": serialized_products.data,
         }
         return render(request, "report.html", context)
     elif request.path == "/reports/inexpensiveproducts":
         products = Product.objects.filter(price__lte=999)
+        serialized_products = ProductSerializer(products, many=True)
         context = {
             "title": "Least Expensive products",
             "heading": "Least Expensive products",
-            "pricey_products": products,
+            "pricey_products": serialized_products.data,
         }
         return render(request, "report.html", context)
